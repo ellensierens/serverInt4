443235a7a3415c9b78f6462b87faea1d3f579bed
ellen sierens <ellen.sierens@student.howest.be>
2020-06-10 17:41:38 +0200

    joystick layout

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const {Board, Servo, Motors} = require("johnny-five");
+const { Board, Servo, Motors } = require("johnny-five");
 let servo;
-let motors
+let motors;
 const invertPWM = true;
 
 const port = process.env.PORT || 8081;
@@ -27,40 +27,52 @@ board.on("ready", function () {
 
   motors = new Motors([
     { pins: { dir: 5, pwm: 6 }, invertPWM },
-    { pins: { dir: 12, pwm: 11}, invertPWM }
+    { pins: { dir: 12, pwm: 11 }, invertPWM },
   ]);
-  console.log('ready');
+  console.log("ready");
 });
 
 io.on("connection", function (socket) {
-  console.log("connected")
+  console.log("connected");
 
-  socket.on("stop", data => {
+  socket.on("stop", (data) => {
     motors.forward(data);
     servo.to(90);
-  })
+  });
 
-  socket.on("carControls", data => {
+  socket.on("carControls", (data) => {
     console.log(`value: ${data.x}`);
-    const scaledX = scale(data.x, -50, 50, 78, 102);
-    console.log(scaledX);
 
-    servo.to(scaledX);
-    // console.log("right");
-
-    // if(data === "stop"){
-    //   motors.forward(0);
-    //   servo.to(90);
-    // }
-
-    if(data.y < 0) {
-      // console.log(`value: ${data.y}`);
-      const scaledY = scale(data.y, -50, 0, 255, 0);
-      motors.forward(scaledY);
-      // console.log(scaledY);
+    if (data.x > 25) {
+      servo.to(78);
+    } else if (data.x < -25) {
+      servo.to(102);
+    } else {
+      const scaledX = scale(data.x, -25, 25, 78, 102);
+      console.log(scaledX);
+      servo.to(scaledX);
     }
 
-    if(data.y > 0) {
+    if (data.y < 0) {
+      if (data.x < -25) {
+        motors.forward(255);
+      } else {
+        // console.log(`value: ${data.y}`);
+        const scaledY = scale(data.y, -25, 0, 255, 0);
+        motors.forward(scaledY);
+        // console.log(scaledY);
+      }
+    }
+
+    if (data.y > 0) {
+      if (data.x > 25) {
+        motors.reverse(255);
+      } else {
+        // console.log(`value: ${data.y}`);
+        const scaledY = scale(data.y, 0, 25, 0, 255);
+        motors.reverse(scaledY);
+        // console.log(scaledY);
+      }
       // console.log(`value: ${data.y}`);
       const scaledY = scale(data.y, 0, 50, 0, 255);
       motors.reverse(scaledY);
@@ -70,7 +82,7 @@ io.on("connection", function (socket) {
 });
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
